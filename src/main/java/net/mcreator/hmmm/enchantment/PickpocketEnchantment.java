862927957724a20a421998e81f273493c707c02c
Ultra-bob <neelgillshah@gmail.com>
2021-07-12 13:52:02 -0400

    added tomato (texture WIP)

---
+++ src/main/java/net/mcreator/hmmm/enchantment/PickpocketEnchantment.java
@@ -0,0 +1,54 @@
+
+package net.mcreator.hmmm.enchantment;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.Enchantment;
+
+import net.mcreator.hmmm.HmmmModElements;
+
+@HmmmModElements.ModElement.Tag
+public class PickpocketEnchantment extends HmmmModElements.ModElement {
+	@ObjectHolder("hmmm:pickpocket")
+	public static final Enchantment enchantment = null;
+	public PickpocketEnchantment(HmmmModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		elements.enchantments.add(() -> new CustomEnchantment(EquipmentSlotType.MAINHAND).setRegistryName("pickpocket"));
+	}
+	public static class CustomEnchantment extends Enchantment {
+		public CustomEnchantment(EquipmentSlotType... slots) {
+			super(Enchantment.Rarity.RARE, EnchantmentType.WEAPON, slots);
+		}
+
+		@Override
+		public int getMinLevel() {
+			return 1;
+		}
+
+		@Override
+		public int getMaxLevel() {
+			return 3;
+		}
+
+		@Override
+		public boolean isTreasureEnchantment() {
+			return true;
+		}
+
+		@Override
+		public boolean isCurse() {
+			return false;
+		}
+
+		@Override
+		public boolean isAllowedOnBooks() {
+			return true;
+		}
+	}
+}
